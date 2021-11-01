14c8c7c62efe16bcb7840ccae7a663abefc972d0
Tatiana-Novikova <tatiana-nova@bk.ru>
2021-11-01 15:54:27 +0000

    Correction-1. Fixed after review

---
+++ middlewares/error-handler.js
@@ -0,0 +1,12 @@
+const { INTERNAL_SERVER_ERROR_CODE } = require('../utils/constants');
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || INTERNAL_SERVER_ERROR_CODE;
+  const message = statusCode === INTERNAL_SERVER_ERROR_CODE
+    ? `На сервере произошла ошибка. ${err}`
+    : err.message;
+  res.status(statusCode).send({ message });
+  next();
+};
+
+module.exports = errorHandler;
