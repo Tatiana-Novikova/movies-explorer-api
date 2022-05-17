f41f3b8c208b147860cc54ba3493d92514ec5a40
Tatiana Novikova <Nusha00793@yandex.ru>
2022-05-17 19:43:46 +0000

    Correction-5. Fixed after review

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ const isEmail = require('validator/lib/isEmail');
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
-    unique: true,
+    required: [true, 'Поле "email" (электронная почта) должно быть заполнено'],
+    unique: [true, 'Поле "email" (электронная почта) должно содержать уникальное значение'],
     validate: {
       validator: (v) => {
         const isValid = isEmail(v);
@@ -16,15 +16,15 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: true,
+    required: [true, 'Поле "password" (пароль) должно быть заполнено'],
     select: false,
   },
   name: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, 'Поле "name" (имя) должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "name" (имя) - 2'],
+    maxlength: [30, 'Максимальная длина поля "name" (имя) - 30'],
   },
-});
+}, { versionKey: false });
 
 module.exports = mongoose.model('user', userSchema);
