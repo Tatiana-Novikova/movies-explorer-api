99a75ada31428d734a76e74b8a3f823694451e7d
Tatiana-Novikova <Nusha00793@yandex.ru>
2022-08-06 01:33:45 +0300

    Correction-10. Fixed after review

--- routes/movies.js
+++ routes/movies.js
@@ -6,10 +6,10 @@ const {
   deleteMovie,
 } = require('../controllers/movies');
 
-const { validateCreateMovie, validateMovieCardId } = require('../middlewares/celebrate-validator');
+const { validateMovieCardId } = require('../middlewares/celebrate-validator');
 
 router.get('/', getMovies);
-router.post('/', validateCreateMovie, createMovie);
+router.post('/', createMovie);
 router.delete('/:id', validateMovieCardId, deleteMovie);
 
 module.exports = router;
